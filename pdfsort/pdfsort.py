cbc9ed20d96e40846e15461d247a94d8305b4957
andygimma <andy.gimma@kiron.ngo>
2017-04-27 01:17:54 -0400

    Generate and write pdfs

--- pdfsort/pdfsort.py
+++ pdfsort/pdfsort.py
@@ -9,7 +9,8 @@ class PDFSort:
             self.pdf_reader = PyPDF2.PdfFileReader(self.pdf, False)
             self.flipped = False
             self.create_temp_pdf()
-            self.pdfPages = self.pdf_reader.numPages
+            self.pdf_pages = self.pdf_reader.numPages
+            self.generated_pages = []
         except IOError:
             print "Could not read file"
     def create_temp_pdf(self):
@@ -31,7 +32,7 @@ class PDFSort:
         return page_numbers
     def flip(self):
         pdf = PyPDF2.PdfFileWriter()
-        for index in range(self.pdfPages):
+        for index in range(self.pdf_pages):
             page = self.pdf_temp.getPage(index)
             page.rotateClockwise(180)
             pdf.addPage(page)
@@ -44,9 +45,37 @@ class PDFSort:
     def rotate_page_numbers(self, pages_string, degrees):
         page_numbers = self.get_page_numbers(pages_string)
         pdf = PyPDF2.PdfFileWriter()
-        for index in range(self.pdfPages):
+        for index in range(self.pdf_pages):
             new_page = self.pdf_temp.getPage(index)
             if index in page_numbers:
                 new_page.rotateClockwise(degrees)
             pdf.addPage(new_page)
         self.pdf_temp = pdf
+    def generate_pdf(self, name, pages_string):
+        ''' Create a new pdf from a given list of pages '''
+        page_numbers = self.get_page_numbers(pages_string)
+        pdf = PyPDF2.PdfFileWriter()
+        for index in range(self.pdf_pages):
+            if index in page_numbers:
+                new_page = self.pdf_temp.getPage(index)
+                pdf.addPage(new_page)
+        self.generated_pages.append({'name': name, 'pdf': pdf})
+    def write_generated_pdfs(self):
+        for pdf in self.generated_pages:
+            pdf_out = open(pdf['name'], 'wb')
+            pdf['pdf'].write(pdf_out)
+            pdf_out.close()
+
+# generate_pdf(name, pages_list)
+# self.generated_pdfs = [{name: pdf1, pages_list: array}]
+# write_generated_pdfs(own_directories=False)
+# split(start, end, increment, name)
+# cli
+# kedsort flip --rotate_pages --tracker --signin --canvass --split_increment
+# ui
+# flip checkbox
+# rotate_pages [list numbers] [degrees]
+# tracker [page_numbers]
+# signin [page_numbers]
+# canvass [page_numbers]
+# split canvass [increment]
