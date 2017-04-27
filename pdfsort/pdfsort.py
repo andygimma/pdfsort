03c24285b24d9827ffd6646904cec12c3c80437d
andygimma <andy.gimma@kiron.ngo>
2017-04-26 21:42:26 -0400

    page_numbers

--- pdfsort/pdfsort.py
+++ pdfsort/pdfsort.py
@@ -1,15 +1,24 @@
 import PyPDF2
 import os
 
-from unittest.mock import mock_ope
-
 class PDFSort:
     def __init__(self, filename):
         try:
             self.filename = filename
-            self.pdf_file = open(input_pdf, 'rb')
-            self.pdf_reader = PyPDF2.PdfFileReader(pdf_in, False)
+            self.pdf = open(self.filename, 'rb')
+            self.pdf_reader = PyPDF2.PdfFileReader(self.pdf, False)
         except IOError:
             print "Could not read file"
         except Exception:
             print "Unknown error"
+    def get_page_numbers(self, string):
+        page_numbers = []
+        raw_list = string.split(',')
+        for group in raw_list:
+            if "-" not in group:
+                page_numbers.append(int(group) - 1)
+            else:
+                t = group.split("-")
+                for i in range(int(t[0]), int(t[1]) + 1):
+                    page_numbers.append(int(i) - 1)
+        return page_numbers
--- test/pdfsort_test.py
+++ test/pdfsort_test.py
@@ -4,8 +4,9 @@ from pdfsort.pdfsort import PDFSort
 class TestPDFSort(unittest.TestCase):
     def test_init(self):
         filename = 'filename.pdf'
-        pdfsort = PDFSort(filename)
-        assert pdfsort.filename == filename
+        pdf_sort = PDFSort(filename)
+        assert pdf_sort.filename == filename
+        assert pdf_sort != None
 
 if __name__ == '__main__':
     unittest.main()
